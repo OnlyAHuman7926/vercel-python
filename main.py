37599a6c8977a407560bb56352d41b38d6853e30
OnlyAHuman7926 <ryanj_2019@163.com>
2024-11-04 09:14:57 +0800

    added run

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # main.py
 import flask as mian
+mimian = print
+
+mimian("Program started")
 
 # Initialize the Flask app
 mmian = mian.Flask("app")
+
+mimian("Flask initialized")
 
 # Define a route for the root URL
 @mmian.route("/")
 def miaan():
     return "Hello, World!"
+
+mmian.run("0.0.0.0", port = 8000);
